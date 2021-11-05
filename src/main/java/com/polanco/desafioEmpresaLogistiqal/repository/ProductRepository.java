b29e1a34b0fbe041076f49ecc57a03a675abbf03
PabPol <pabpolval@gmail.com>
2021-11-05 18:31:48 -0300

    PagingAndSortingRepository añadido para paginación

--- src/main/java/com/polanco/desafioEmpresaLogistiqal/repository/ProductRepository.java
+++ src/main/java/com/polanco/desafioEmpresaLogistiqal/repository/ProductRepository.java
@@ -1,11 +1,17 @@
 package com.polanco.desafioEmpresaLogistiqal.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import com.polanco.desafioEmpresaLogistiqal.model.Product;
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 	
+	public List<Product> findByProductName(String productName);
 	
 
 }
